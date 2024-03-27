b7a743be024f36531ebaead44030398d849e3f17
Orsefinance <93208228+Orsefinance@users.noreply.github.com>
2024-03-27 22:52:22 +0300

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -34,12 +34,12 @@ const greeting = {
 
 const socialMediaLinks = {
  
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  //linkedin: "https://www.linkedin.com/in/saadpasta/",
+  //gmail: "saadpasta70@gmail.com",
+  //gitlab: "https://gitlab.com/saadpasta",
+  //facebook: "https://www.facebook.com/saad.pasta7",
+  //medium: "https://medium.com/@saadpasta",
+  //stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
@@ -363,8 +363,8 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+  number: "+90-5524724037",
+  email_address: "charset.ggo@gmail.com"
 };
 
 // Twitter Section
