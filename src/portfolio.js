2aea53fbac2c7c077c83a0efce8d86a76b0bf467
Orsefinance <93208228+Orsefinance@users.noreply.github.com>
2024-04-25 18:35:04 +0300

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const illustration = {
 
 const greeting = {
   username: "Clothers",
-  title: "Hi all, I'm Chother LTD",
+  title: "Chother LTD",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
